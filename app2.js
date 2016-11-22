2288cca1e28af696d11734ecff59b09207ce2cc4
Kasper Helsted <khelst13@student.aau.dk>
2016-11-22 14:24:51 +0100

    Login 'works'

--- app2.js
+++ app2.js
@@ -1,32 +1,36 @@
 $('document').ready(function() {
-    $('.login-form').submit(function(){
-        //alert($('input[name=username]').val());
-        //alert($('input[name=password]').val());
+    if (localStorage.getItem("token") != null)
+    {
+        window.location="dashboard.html";
+    }else
+    {
+        $('.login-form').submit(function(){
+            $('.login-message').stop(true, false).slideUp('veryfast', function(){
+                url = "http://172.25.11.114:8080/services-1.0.0/auth"
 
-        $('.login-message').stop(true, false).slideUp('veryfast', function(){
-            url = "http://172.25.11.114:8080/services-1.0.0/auth"
+                $.ajax({
+                    url: url,
+                    type: 'post',
+                    data: JSON.stringify({
+                        username: $('input[name=username]').val(),
+                        password: $('input[name=password]').val()
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    dataType: 'json',
+                    success: function (data) {
+                        localStorage.setItem("token", data["token"]);
+                        window.location="dashboard.html";
+                    }
+                });
 
-            $.ajax({
-                url: url,
-                type: 'post',
-                data: {
-                    username: $('input[name=username]').val(),
-                    password: $('input[name=password]').val()
-                },
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                dataType: 'json',
-                success: function (data) {
-                    console.info(data);
-                }
+
+                $('.login-message p').html($('input[name=username]').val());
+                $('.login-message').stop(true, false).slideDown('veryfast');
             });
 
-
-            $('.login-message p').html($('input[name=username]').val());
-            $('.login-message').stop(true, false).slideDown('veryfast');
+            return false;
         });
-
-        return false;
-    });
+    }
 });
