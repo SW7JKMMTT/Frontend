0730e4750ce5ffc67764ad71660842f647621f1f
Kasper Helsted <khelst13@student.aau.dk>
2016-11-28 15:16:29 +0100

    Login init commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,52 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'nuts',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: "html/app.html"
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class LoginComponent  { 
+    constructor(private http: Http) { }
+
+    login(form: any){
+        return this.getToken(form.username, form.password)
+            .subscribe(
+                data => {
+                    data = data.json();
+                    console.log(data)
+                },
+                error => {
+                    console.log("Error" + error);
+                }
+            )
+
+        // let url = "http://192.168.1.241:8080/services-1.0.0/auth";
+
+        // let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        // let options = new RequestOptions({ headers: headers });
+
+        // var username = form.username;
+        // var password = form.password;
+
+
+        // let parameters = "username=" + username + "&password=" + password;
+
+        // console.log(this.http.post(url, parameters, options).map(res => res.json()));
+    }
+
+    getToken(username: string, password: string) {
+        let url = "72e5497d.ngrok.io/services-1.0.0/auth";
+
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        let parameters = JSON.stringify({
+            "username": username,
+            "password": password
+        });
+
+        return this.http.post(url, parameters, options);
+    }
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { HttpModule }   from '@angular/http';
 
-import { AppComponent }  from './app.component';
+import { LoginComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, HttpModule, FormsModule ],
+  declarations: [ LoginComponent ],
+  bootstrap:    [ LoginComponent ]
 })
 export class AppModule { }
